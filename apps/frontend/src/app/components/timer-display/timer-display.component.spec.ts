2307cbd57d9ecf88214c23133d2b57a3a385ba09
SoftwareKater <softwarekater@gmail.com>
2020-11-29 17:20:24 +0100

    style main component

--- apps/frontend/src/app/components/timer-display/timer-display.component.spec.ts
+++ apps/frontend/src/app/components/timer-display/timer-display.component.spec.ts
@@ -1,41 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ScoreBoardComponent } from './score-board.component';
+import { TimerDisplayComponent } from './timer-display.component';
 
-describe('ScoreBoardComponent', () => {
-  let component: ScoreBoardComponent;
-  let fixture: ComponentFixture<ScoreBoardComponent>;
+describe('TimerDisplayComponent', () => {
+  let component: TimerDisplayComponent;
+  let fixture: ComponentFixture<TimerDisplayComponent>;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [ScoreBoardComponent],
+      declarations: [TimerDisplayComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ScoreBoardComponent);
+    fixture = TestBed.createComponent(TimerDisplayComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  describe('score', () => {
-    it('should deal with bad inputs', () => {
-      component.player1Score = '';
-      component.score = null;
-      expect(component.player1Score).toBe('00');
-
-      component.player2Score = '';
-      component.score = undefined;
-      expect(component.player2Score).toBe('00');
-    });
-
-    it('should set player 1 and player 2 score', () => {
-      component.score = { player1: 3, player2: 8 };
-      expect(component.player1Score).toBe('03');
-      expect(component.player2Score).toBe('08');
-    });
   });
 });
